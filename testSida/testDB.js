86dd1c246b0e68ea075ae2ca6c6786217d63ce6b
Fleron <emil.fleron@gmail.com>
2015-12-01 23:44:21 +0100

    simple working payments and inventory adding

--- testSida/testDB.js
+++ testSida/testDB.js
@@ -1,20 +1,55 @@
 "use strict"
-$(document).ready(function(){
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get',
+        dataType: "json",
+        async: true,
+        success: function (result) {
+            for (var i = 2; result.payload.length; i++) {
+                $('#lager').append(
+                    '<tr><td>' + result.payload[i].namn +
+                    '</td><td>' + result.payload[i].pub_price +
+                    '</td><td>' + result.payload[i].count +
+                    '</td><td>' + result.payload[i].beer_id +
+                    '</td></tr>')
+            }
+        },
+        error: function () {
+            alert('error loading')
+        }
+    });
+    $('#dbButton').click(function () {
         $.ajax({
             type: 'GET',
-            url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get',
+            url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=purchases_append&beer_id=157503',
             dataType: "json",
             async: true,
-            success: function(result){
+            success: function (result) {
+                location.reload(true)
                 console.log(result)
-                console.log(result.payload[0].namn)
-                for(var i=2; result.payload.length; i++){
-                    $('#lager').append('<tr><td>'+result.payload[i].namn+'</td><td>'+result.payload[i].pub_price+'</td><td>'+result.payload[i].count+'</td></tr>')
-                }
+                console.log("buy confirm")
+
             },
-            error: function(){
+            error: function () {
                 alert('error loading')
             }
         });
     });
+    $('#dbButton2').click(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_append&beer_id=157503&amount=10&price=25',
+            dataType: "json",
+            async: true,
+            success: function (result) {
+                location.reload(true)
+                console.log("add confirm")
+            },
+            error: function () {
+                alert('error loading')
+            }
+        });
+    });
+});
 
