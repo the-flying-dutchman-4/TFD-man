83c546818c5eaeba8cd441ecca6810a7eac71a9a
JihengChen <cjhcjh2014@gmail.com>
2015-12-02 15:42:01 +0100

    drag and drop is ready in CustomerMain.html

--- Cart.js
+++ Cart.js
@@ -74,3 +74,25 @@ function deleteProduct(name, price)
 		}
 	}		
 }
+
+function allowDrop(ev) 
+{
+    ev.preventDefault();
+}
+
+function drag(ev) 
+{
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) 
+{
+    ev.preventDefault();
+ 
+    var data = ev.dataTransfer.getData("text");
+
+    var name = Number(data[4]);
+    addProduct(name, 5);
+    // ev.target.appendChild(document.getElementById(data));
+ 
+}
