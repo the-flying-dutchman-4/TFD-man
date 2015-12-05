36a763c57f312f407e09528cbef993a8983bb7b6
Fleron <emil.fleron@gmail.com>
2015-12-05 14:11:05 +0100

    update

--- javascript/login.js
+++ javascript/login.js
@@ -13,21 +13,14 @@ $(document).ready(function(){
             async: true,
             success: function(result){
                 if(result.type != "error"){
-                    console.log("hej")
-                    console.log(result.payload[0].assets)
-                    textDisp.append("|name: " + result.payload[0].first_name + " last name: " + result.payload[0].last_name)
-                    console.log(result.payload[0].user_id)
-                    console.log(admins)
                     var temp = parseInt(result.payload[0].user_id, 10)
-                    console.log(temp)
                     var temp2 = $.inArray(temp, admins)
-                    console.log(temp2)
-                    //if (temp2 > -1) {
-                    //    $(location).attr('href', 'MenuList.html')
-                    //}
-                    //else {
-                    //    $(location).attr('href', 'CustomerMain.html')
-                    //}
+                    if (temp2 > -1) {
+                        $(location).attr('href', 'MenuListJan.html')
+                    }
+                    else {
+                        $(location).attr('href', 'CustomerMain_use.html')
+                    }
                 }
                 else{
                     console.log("error");
